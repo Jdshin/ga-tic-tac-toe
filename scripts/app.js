15b3fdca2b3006f286ead4a35b30bc60a54edb8a
Jdshin <jddshin@gmail.com>
2021-12-23 11:38:24 -0500

    endGame function now clears board and removes event listeners

--- scripts/app.js
+++ scripts/app.js
@@ -1,21 +1,19 @@
+let currMoveNum = 0;
+const $squares = $('.square');
+
 const startBut = $('.startBut');
 const endBut = $('.endBut');
-
 startBut.on('click', startGame);
 endBut.on('click', endGame);
 
-let currMoveNum = 0;
-
 function startGame(){
     console.log("Game started");
-    const $squares = $('.square');
-    let testSquare = $('#sq9');
-    console.log($squares);
     $squares.on('click', onSquareClick);
 }
 
 function endGame(){
-
+    $squares.off('click');
+    $squares.html("");
 }
 
 function onSquareClick(){
