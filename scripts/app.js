8061541bed1ccde1172803c129d4d02fc385daf7
Jdshin <jddshin@gmail.com>
2021-12-23 11:04:52 -0500

    Add initial startGame, adds event handlers to squares

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,20 @@
-function getMarker(i){
-    return (i%2==0 ? 'X' : 'O');
-}
-
 const startBut = $('.startBut');
 const endBut = $('.endBut');
 
-startBut.on('click', function(){console.log("Game started")});
-endBut.on('click', function(){console.log("Game ended")});
+startBut.on('click', startGame);
+endBut.on('click', endGame);
 
+function startGame(){
+    console.log("Game started");
+    const $squares = $('.square');
+    const $numSquares = $squares.length;
+    $squares.on('click', function(){console.log(this.id)});
+}
+
+function endGame(){
+
+}
+
+function getMarker(i){
+    return (i%2==0 ? 'X' : 'O');
+}
