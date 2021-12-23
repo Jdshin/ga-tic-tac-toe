079a63f3f65d092ffdd2e768de4ab25386a2f8db
Jdshin <jddshin@gmail.com>
2021-12-23 12:20:58 -0500

    Add win condition check for rows

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,11 @@
 let currMoveNum = 1;
+let sideLength = 3;
 const $squares = $('.square');
 
 const startBut = $('.startBut');
 const endBut = $('.endBut');
 startBut.on('click', startGame);
 endBut.on('click', endGame);
-
-let winConditions = [
-    []
-]
 
 function startGame(){
     console.log("Game started");
@@ -23,8 +20,30 @@ function endGame(){
 function onSquareClick(){
     let $this = $(this);
     this.innerHTML = getMarker(currMoveNum);
-    currMoveNum += 1;
     $this.off('click');
+    checkWin();
+    currMoveNum += 1;
+}
+
+function checkWin(){
+    if (currMoveNum > 4){
+        const player = getMarker(currMoveNum);
+        console.log(`Player ${player}`);
+        // Check rows
+        for (let i = 0; i < $squares.length; i += sideLength){
+            const first = $squares.get(i);
+            const second = $squares.get(i+1);
+            const third = $squares.get(i+2);
+            if (first.innerHTML == player && second.innerHTML == player && third.innerHTML == player){
+                console.log(`Player ${player} wins!`);
+                endGame();
+            }
+        }
+        // Check columns
+
+        // Check diagonals
+    }
+    
 }
 
 function getMarker(i){
