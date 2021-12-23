b699d1d25286ca37a32847bf3874d6feef29fe51
Jdshin <jddshin@gmail.com>
2021-12-23 10:48:18 -0500

    Add start and quit game buttons with prelim event handlers

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,10 @@
-let board = document.querySelector(".board");
-
 function getMarker(i){
     return (i%2==0 ? 'X' : 'O');
 }
+
+const startBut = $('.startBut');
+const endBut = $('.endBut');
+
+startBut.on('click', function(){console.log("Game started")});
+endBut.on('click', function(){console.log("Game ended")});
+
